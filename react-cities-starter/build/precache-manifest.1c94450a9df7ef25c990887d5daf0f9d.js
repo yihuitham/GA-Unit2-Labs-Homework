4d82bf65373026d52cee3d72474af69f26f771d4
YihuiTham <yihuitham@gmail.com>
2021-10-13 23:11:38 +0800

    remove .gitignore from cities folder

---
+++ react-cities-starter/build/precache-manifest.1c94450a9df7ef25c990887d5daf0f9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fc71f5faa3b6cd4b7617bc322f041a0",
+    "url": "/react-cities-starter/index.html"
+  },
+  {
+    "revision": "a1685843b8c100f03de3",
+    "url": "/react-cities-starter/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "06119e33fab45d903436",
+    "url": "/react-cities-starter/static/js/2.f9d82fd4.chunk.js"
+  },
+  {
+    "revision": "a1685843b8c100f03de3",
+    "url": "/react-cities-starter/static/js/main.754986b1.chunk.js"
+  },
+  {
+    "revision": "c73938739d0f5e53399e",
+    "url": "/react-cities-starter/static/js/runtime~main.b95ab25e.js"
+  }
+]);
