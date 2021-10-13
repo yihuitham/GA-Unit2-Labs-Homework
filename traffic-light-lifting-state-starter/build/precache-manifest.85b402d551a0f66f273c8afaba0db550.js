12c5e72a7b2292e42f34fd53bece25a813252f82
YihuiTham <yihuitham@gmail.com>
2021-10-13 23:30:45 +0800

    deploy traffic-light

---
+++ traffic-light-lifting-state-starter/build/precache-manifest.85b402d551a0f66f273c8afaba0db550.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e5460060a178a25ad0934c3e28751ec",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3eab4e1f7194c0609eb4",
+    "url": "./static/css/main.fcd60dfc.chunk.css"
+  },
+  {
+    "revision": "06119e33fab45d903436",
+    "url": "./static/js/2.f9d82fd4.chunk.js"
+  },
+  {
+    "revision": "3eab4e1f7194c0609eb4",
+    "url": "./static/js/main.d7e1b2c8.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
