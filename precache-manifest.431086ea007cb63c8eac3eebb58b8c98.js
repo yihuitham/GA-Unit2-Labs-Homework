b31603fc0f81235405cf20d8ef4ec05eaf46fa1f
YihuiTham <yihuitham@gmail.com>
2021-10-14 18:44:58 +0800

    Updates

---
+++ precache-manifest.431086ea007cb63c8eac3eebb58b8c98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd049e08011f4598aec68a4d35451b6e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3d718da0bcb722ab5346",
+    "url": "./static/css/main.c506c6ee.chunk.css"
+  },
+  {
+    "revision": "98ad9538443952f13f8f",
+    "url": "./static/js/2.eee01206.chunk.js"
+  },
+  {
+    "revision": "3d718da0bcb722ab5346",
+    "url": "./static/js/main.3dd52347.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
