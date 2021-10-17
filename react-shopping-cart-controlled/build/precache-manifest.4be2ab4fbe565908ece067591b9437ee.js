ea2f8eaf052dc507a4457940e5c77b4a50609c41
YihuiTham <yihuitham@gmail.com>
2021-10-17 11:23:25 +0800

    build controlled form

---
+++ react-shopping-cart-controlled/build/precache-manifest.4be2ab4fbe565908ece067591b9437ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03442f1ae6f230cf8fdce533f735cf9b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ddf161a2cb657b038958",
+    "url": "./static/css/main.c506c6ee.chunk.css"
+  },
+  {
+    "revision": "4cc87cd608f2e321539c",
+    "url": "./static/js/2.f644d55a.chunk.js"
+  },
+  {
+    "revision": "ddf161a2cb657b038958",
+    "url": "./static/js/main.0262d5dc.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
