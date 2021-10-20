35c0d0000446e480c6bb5e0ce6c1ecfd0cb7ccfd
YihuiTham <yihuitham@gmail.com>
2021-10-20 22:56:44 +0800

    add useContext themes app

---
+++ usecontext-lab-themes/build/precache-manifest.6fb2511208fb11a5d7263ae38119ccd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b2fe04e0361a4f339fb7d72d36a6f35",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1ede4abc4fe85d5c51ce",
+    "url": "./static/css/main.efffb38f.chunk.css"
+  },
+  {
+    "revision": "662daf0185e68975c0c9",
+    "url": "./static/js/2.5206bdd1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.5206bdd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ede4abc4fe85d5c51ce",
+    "url": "./static/js/main.14063f35.chunk.js"
+  },
+  {
+    "revision": "81b8bc68133e3537da2e",
+    "url": "./static/js/runtime-main.f56b2b77.js"
+  }
+]);
--- usecontext-lab-themes/build/service-worker.js
+++ usecontext-lab-themes/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.55175c835118534bd59b41069d41143d.js"
+  "./precache-manifest.6fb2511208fb11a5d7263ae38119ccd0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
