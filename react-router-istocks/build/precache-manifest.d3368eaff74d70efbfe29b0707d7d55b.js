e4c40d1e3281ff0bfbecc730dd18cae0bf585a39
YihuiTham <yihuitham@gmail.com>
2021-10-25 19:39:45 +0800

    add react-router-iStocks

---
+++ react-router-istocks/build/precache-manifest.d3368eaff74d70efbfe29b0707d7d55b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97d9342950d9e82b76d0dbad8e99eed5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fe640c9fbdb47553956",
+    "url": "/static/css/main.59b9d325.chunk.css"
+  },
+  {
+    "revision": "efacdd9bf7d1d9d14395",
+    "url": "/static/js/2.571e0ee3.chunk.js"
+  },
+  {
+    "revision": "9fe640c9fbdb47553956",
+    "url": "/static/js/main.9421b30b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
